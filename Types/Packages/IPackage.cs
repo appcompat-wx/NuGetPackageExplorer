362d2d5a5fa979b534551eb2941a9ab289c38160
appcompat-wx <appcompatwx@microsoft.com>
2026-04-23 15:06:53 +0800

    upload code from lab

---
+++ Types/Packages/IPackage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using NuGet.Packaging;
+
+namespace NuGetPe
+{
+    public interface IPackage : IPackageMetadata, IServerPackageMetadata, IDisposable
+    {
+        bool IsAbsoluteLatestVersion { get; }
+
+        bool IsLatestVersion { get; }
+
+        bool IsPrerelease { get; }
+
+        DateTimeOffset LastUpdated { get; }
+
+        DateTimeOffset? Published { get; }
+
+        IEnumerable<IPackageFile> GetFiles();
+
+        Stream GetStream();
+    }
+}
